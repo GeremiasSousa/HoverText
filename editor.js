41ab899572022d9953aeb80cdb5fda95e52f9087
geremiasSousa <sousageremias9@Gmail.com>
2023-03-15 21:58:47 -0300

    o alinhamento dos textos

--- editor.js
+++ editor.js
@@ -1,27 +1,14 @@
 const content_editor = document.querySelector("#editor");
 
 
-
 document.getElementById('center').addEventListener('click', ()=> textAlign('center'));
 document.getElementById('justify').addEventListener('click', ()=> textAlign('justify'));
 document.getElementById('left').addEventListener('click', ()=> textAlign('left'));
 document.getElementById('right').addEventListener('click', ()=> textAlign('right'));
 
-function text() {
-    let res_tag = content_editor.innerText.split('\n')
-        .filter(content => content.trim())
-        .map((content, count) => `<p id='${count}'>${content}</p>`);
-    content_editor.innerHTML = '';
-    res_tag.forEach(element => {
-        content_editor.innerHTML += element;
-    });
-}
-
 
 function textAlign(text_align){
-    text();
     let selection = document.getSelection();
-    console.log(getSelection());
-    // let element_selection = selection.anchorNode.parentElement;
-    // element_selection.style.textAlign = text_align;
+    let element_selection = selection.anchorNode.parentElement;
+    element_selection.style.textAlign = text_align;
 }
